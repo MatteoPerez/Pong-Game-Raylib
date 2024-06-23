033c04a138049cdf0c79525e60ecf71717ea4c2c
MatteoPerez <matteo.perez@student.junia.com>
2024-06-23 12:00:44 +0200

    Ajout mouvements du joueur

--- main.cpp
+++ main.cpp
@@ -25,7 +25,34 @@ class Ball{
     }
 };
 
+class Paddle{
+    public:
+    float x, y;
+    float width, height;
+    int speed;
+
+    void Draw(){
+        DrawRectangle(x, y, width, height, WHITE);
+    }
+    
+    void Update(){
+        if(IsKeyDown(KEY_UP)){
+            y = y - speed;
+        }
+        if(IsKeyDown(KEY_DOWN)){
+            y = y + speed;
+        }
+        if(y <= 0){
+            y = 0;
+        }
+        if(y + height >= GetScreenHeight()){
+            y = GetScreenHeight() - height;
+        }
+    }
+};
+
 Ball ball;
+Paddle player;
 
 int main() {    
     cout << "Starting game !" << endl;
@@ -41,18 +68,25 @@ int main() {
     ball.speed_x = 7;
     ball.speed_y = 7;
 
+    player.width = 25;
+    player.height = 120;
+    player.x = 10;
+    player.y = screen_height/2 - player.height/2;
+    player.speed = 6;
+
     while(WindowShouldClose() == false){
         BeginDrawing();     // Create a blank canvas
         
         // Updating
         ball.Update();
+        player.Update();
 
         // Drawing
         ClearBackground(BLACK);     // Fill window with black color (to clear it)
         DrawLine(screen_width/2, 0, screen_width/2, screen_height, WHITE);
         ball.Draw();
-        DrawRectangle(10, screen_height/2 - 60, 25, 120, WHITE);
         DrawRectangle(screen_width - 35, screen_height/2 - 60, 25, 120, WHITE);
+        player.Draw();
         EndDrawing();
     }
 
